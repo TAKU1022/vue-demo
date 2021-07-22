b5d05cbb399d950b62ce30f18c6fe806fdfbd7c9
Takumi <nase49946@gmail.com>
2021-07-22 23:50:19 +0900

    関数を使用した省略記法

--- src/main.js
+++ src/main.js
@@ -4,12 +4,9 @@ import LikeNumber from './components/LikeNumber.vue';
 
 Vue.config.productionTip = false;
 Vue.component('LikeNumber', LikeNumber);
-Vue.directive('border', {
-  bind() {},
-  inserted() {},
-  update() {},
-  componentUpdated() {},
-  unbind() {},
+Vue.directive('border', function (el, binding) {
+  el.style.border = '1px solid black';
+  el.style.borderWidth = binding.value;
 });
 
 new Vue({
