b5265de46aadbaf2d986939f5c34ee35d3242f85
Takumi <nase49946@gmail.com>
2021-07-23 15:39:22 +0900

    フィルターを使用したデータ変換

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,33 @@
 <template>
-  <p v-border:solid.round="{ width: '3px', color: 'brown' }">Home</p>
+  <div>
+    <p v-border:solid.round="{ width: '3px', color: 'brown' }">Home</p>
+    <h2>{{ title | lowerCase | upperCase }}</h2>
+    <p>{{ subTitle | upperCase | lowerCase }}</p>
+  </div>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        title: 'Welcome to Tokyo',
+        subTitle: 'Tokyo is a great city',
+      };
+    },
+    filters: {
+      lowerCase(value) {
+        return value.toLowerCase();
+      },
+    },
+    directives: {
+      border(el, binding) {
+        el.style.borderWidth = binding.value.width;
+        el.style.borderColor = binding.value.color;
+        el.style.borderStyle = binding.arg;
+        if (binding.modifiers.round) {
+          el.style.borderRadius = '0.5em';
+        }
+      },
+    },
+  };
+</script>
--- src/main.js
+++ src/main.js
@@ -4,14 +4,17 @@ import LikeNumber from './components/LikeNumber.vue';
 
 Vue.config.productionTip = false;
 Vue.component('LikeNumber', LikeNumber);
-Vue.directive('border', function (el, binding) {
-  el.style.borderWidth = binding.value.width;
-  el.style.borderColor = binding.value.color;
-  el.style.borderStyle = binding.arg;
-  if (binding.modifiers.round) {
-    el.style.borderRadius = '0.5em';
-  }
+Vue.filter('upperCase', (value) => {
+  return value.toUpperCase();
 });
+// Vue.directive('border', (el, binding) => {
+//   el.style.borderWidth = binding.value.width;
+//   el.style.borderColor = binding.value.color;
+//   el.style.borderStyle = binding.arg;
+//   if (binding.modifiers.round) {
+//     el.style.borderRadius = '0.5em';
+//   }
+// });
 
 new Vue({
   render: (h) => h(App),
